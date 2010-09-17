9121657a29d3732eada063dad05adbdb38c39a35
dtchepak <dave@davesquared.net>
2010-09-17 23:02:08 +1000

    Adding stub StringCalculator class.

---
+++ src/StringCalculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DaveSquared.StringsTheThing
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            throw new NotImplementedException("hmm, needs some work here.");
+        }
+    }
+}
