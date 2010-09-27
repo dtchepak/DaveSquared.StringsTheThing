67c1137beba8c52497cf4c54b9ea487836a27cc7
dtchepak <dave@davesquared.net>
2010-09-27 22:53:37 +1000

    Adding just expression parts parser class and specs.

---
+++ src/ExpressionPartsParser.cs
@@ -0,0 +1,23 @@
+﻿namespace DaveSquared.StringsTheThing
+{
+    public class ExpressionPartsParser : IExpressionPartsParser
+    {
+        public ExpressionParts Parse(string expression)
+        {
+            if (!expression.StartsWith("//")) return GetPartsWithDefaultDelimiter(expression);
+            return GetPartsWithCustomDelimiter(expression);
+        }
+
+        ExpressionParts GetPartsWithCustomDelimiter(string expression)
+        {
+            var delimiter = expression[2];
+            var remainder = expression.Substring(4);
+            return new ExpressionParts(delimiter, remainder);
+        }
+
+        ExpressionParts GetPartsWithDefaultDelimiter(string expression)
+        {
+            return new ExpressionParts(',', expression);
+        }
+    }
+}
---
+++ src/Specs/ExpressionPartsParserSpecs.cs
@@ -0,0 +1,70 @@
+using System;
+using NUnit.Framework;
+
+namespace DaveSquared.StringsTheThing.Specs
+{
+    public class ExpressionPartsParserSpecs
+    {
+        public class When_parsing_expression_parts : ConcernFor<ExpressionPartsParser>
+        {
+            protected ExpressionParts result;
+            protected string expression;
+
+            protected override void Because()
+            {
+                result = Subject.Parse(expression);
+            }
+
+            protected override ExpressionPartsParser CreateSubject()
+            {
+                return new ExpressionPartsParser();
+            }
+        }
+
+        public class WhenParsingExpressionPartsFromAnExpressionWithACustomDelimiter:When_parsing_expression_parts
+        {
+            char delimiter;
+            string remainder;
+
+            [Test]
+            public void Return_the_delimiter_from_the_expression()
+            {
+                Assert.That(result.Delimiter, Is.EqualTo(delimiter));
+            }
+
+            [Test]
+            public void Return_the_remainder_of_the_expression_to_parse()
+            {
+                Assert.That(result.Remainder, Is.EqualTo(remainder));
+            }
+
+            protected override void Context()
+            {
+                delimiter = ';';
+                remainder = "the rest of the expressoin";
+                expression = string.Format("//{0}\n{1}", delimiter, remainder);
+            }
+        }
+
+        public class When_parsing_parts_from_an_expression_without_a_custom_delimiter : When_parsing_expression_parts {
+
+            [Test]
+            public void Return_comma_as_the_default_delimiter()
+            {
+                Assert.That(result.Delimiter, Is.EqualTo(','));
+            }
+
+            [Test]
+            public void Return_the_whole_expression_as_the_remainder_to_parse()
+            {
+                Assert.That(result.Remainder, Is.EqualTo(expression));
+            }
+
+            protected override void Context()
+            {
+                expression = "the expression";
+            }
+        }
+    }
+
+}
